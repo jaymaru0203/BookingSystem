a5feaa695f29108762cb973f8403c62782711a50
Esha Vats <56391290+eshavats@users.noreply.github.com>
2020-11-26 06:41:12 +0530

    events page complete, movies page partially complete

--- movies.php
+++ movies.php
@@ -5,7 +5,7 @@
 require('includes/config.php');
 
 ?>
-    <title>Home</title>
+    <title>Movies</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
@@ -15,7 +15,9 @@ require('includes/config.php');
       integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
       crossorigin="anonymous"
     />
-    <link rel="stylesheet" href="homepage.css" />
+    
+    <!-- Font Awesome -->
+    <script src="https://kit.fontawesome.com/aeaca870f5.js" crossorigin="anonymous"></script>
 
     <script
       src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
@@ -59,7 +61,7 @@ require('includes/config.php');
 
     /* Carousel */
     .carousel-inner img {
-      height: 23em;
+      height: 24em;
     }
 
     /* Event Cards Container */
@@ -106,10 +108,52 @@ require('includes/config.php');
       height: 30px;
       padding: 0 10px;
     }
+    h3 {
+        display: inline-block;
+    }
+    #events {
+        text-align: left !important;
+
+    }
+    .sort-div {
+        text-align: right;
+    }
+    #sort {
+        max-width: 80% !important;
+    }
+    .btn-custom {
+        background-color: #1f1f1f;
+        color: white;
+    }
+    .btn-custom:hover {
+        background-color: grey;
+        color: white;
+
+    }
+    .jumbotron {
+        width: 72rem !important;
+        padding: 1rem !important;
+        margin-top: 1%;
+    }
+    #sortForm label {
+        font-size: 1.2rem;
+    }
+
     </style>
   <body>
-    <!-- Navbar -->
+  <?php
+    $sort = $filter = "";
 
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if(!empty($_POST["sort"])) {
+            $sort = $_POST["sort"];
+        } else if(!empty($_POST["filter"])) {
+            $filter = $_POST["filter"];
+        }
+
+  } ?>
+    
+    <!-- Navbar -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <button
         class="navbar-toggler"
@@ -132,15 +176,14 @@ require('includes/config.php');
           />
         </a>
         <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" href="homepage.php"
+              >Home </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="events.php">Events<span class="sr-only">(current)</span></a>
+          </li>
           <li class="nav-item active">
-            <a class="nav-link" href="#"
-              >Home <span class="sr-only">(current)</span></a
-            >
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="events.php">Events</a>
-          </li>
-          <li class="nav-item">
             <a class="nav-link" href="movies.php">Movies</a>
           </li>
           <li class="nav-item">
@@ -193,27 +236,13 @@ require('includes/config.php');
         </ol>
         <div class="carousel-inner">
           <div class="carousel-item active">
-            <img src="./images/concert2.jpg" class="d-block w-100" alt="..." />
-            <div class="carousel-caption d-none d-md-block">
-              <h5>Beatnik Summer Festival</h5>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </div>
+            <img src="./images/sale2.jpg" class="d-block w-100" alt="..." />
           </div>
           <div class="carousel-item">
-            <img src="./images/concert4.jpg" class="d-block w-100" alt="..." />
-            <div class="carousel-caption d-none d-md-block">
-              <h5>Acapella Events</h5>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </div>
+            <img src="./images/sale4.jpg" class="d-block w-100" alt="..." />
           </div>
           <div class="carousel-item">
-            <img src="./images/concert5.jpg" class="d-block w-100" alt="..." />
-            <div class="carousel-caption d-none d-md-block">
-              <h5>Sound Spirit Fest</h5>
-              <p>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-              </p>
-            </div>
+            <img src="./images/sale5.jpg" class="d-block w-100" alt="..." />
           </div>
         </div>
         <a
@@ -240,12 +269,125 @@ require('includes/config.php');
     <!-- Event Cards -->
 
     <div class="container text-center event-container">
-      <h3 id="events" class="event-category">Events</h3>
+        <div id="events">
+        <div class="row">
+        <div class="col-2">
+      <h3  class="event-category">Movies</h3>
+      </div>
+
+
+  <div id="filter" class="col-10 sort-div">
+  <a class="btn btn-custom btn-md" data-toggle="collapse" href="#multiCollapseExample1" role="button" aria-expanded="false" aria-controls="multiCollapseExample1"><i class="fas fa-sort-amount-down"></i> Sort By</a>
+
+  <button class="btn btn-custom btn-md" type="button" data-toggle="collapse" data-target="#multiCollapseExample2" aria-expanded="false" aria-controls="multiCollapseExample2"><i class="fas fa-filter"></i> Filter By</button>
+  </div>
+  </div>
+<div class="row">
+    <div class="collapse" id="multiCollapseExample1">
+    <div class="jumbotron">
+
+    <form id="sortForm" name="sort" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
+
+    <div class="row">
+    <div class="col-2">
+    <label>
+    <input type="checkbox" name="sort" value="eventPriceLow"> Price <i class="fas fa-sort-numeric-down"></i>
+    </label>
+    </div>
+    <div class="col-2">
+    <label>
+    <input type="checkbox" name="sort" value="eventPriceHigh"> Price <i class="fas fa-sort-numeric-up-alt"></i></i>
+    </label>
+    </div>
+    <div class="col-2">
+    <label>
+    <input type="checkbox" name="sort" value="eventDateTimeLow"> Date <i class="fas fa-sort-numeric-down"></i>
+    </label>
+    </div>
+    <div class="col-2">
+    <label>
+    <input type="checkbox" name="sort" value="eventDateTimeHigh"> Date <i class="fas fa-sort-numeric-up-alt"></i>
+    </label>
+    </div>
+    <div class="col-2">
+    <label>
+    <input type="checkbox" name="sort" value="eventNameLow"> Title <i class="fas fa-sort-alpha-down"></i>
+    </label>
+    </div>
+    <div class="col-2">
+    <button type="submit" class="btn btn-danger">Sort Movies</button>
+    </div>
+    </div>
+
+</form>
+</div>
+  </div>
+    <div class="collapse" id="multiCollapseExample2">
+    <div class="jumbotron">
+
+    <form id="filterForm" name="filter" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
+    <h5>Genre</h5>
+    <div class="row">
+
+    <div class="col-2">
+    <label>
+    <input type="checkbox" name="filter" value="Action"> Action
+    </label>
+    </div>
+    <div class="col-2">
+    <label>
+    <input type="checkbox" name="filter" value="Comedy"> Comedy
+    </label>
+    </div>
+    <div class="col-2">
+    <label>
+    <input type="checkbox" name="filter" value="Drama"> Drama
+    </label>
+    </div>
+    <div class="col-2">
+    <label>
+    <input type="checkbox" name="filter" value="Romance"> Romance
+    </label>
+    </div>
+    <div class="col-2">
+    <label>
+    <input type="checkbox" name="filter" value="Sci-Fi"> Sci-Fi
+    </label>
+    </div>
+    <div class="col-2">
+    <button type="submit" class="btn btn-danger">Filter Movies</button>
+    </div>
+    </div>
+
+    </form>
+    </div>
+  </div>
+      </div>
+      </div>
       <br />
       <div class="row flex-container">
       <?php
 
-    $sql = "SELECT * FROM events";
+if($sort != "") {
+    if($sort == "eventPriceLow")
+    {
+        $sql = "SELECT * FROM events ORDER BY eventPrice ASC";
+    } else if($sort == "eventPriceHigh") {
+        $sql = "SELECT * FROM events ORDER BY eventPrice DESC";
+    } else if($sort == "eventDateTimeLow") {
+        $sql = "SELECT * FROM events ORDER BY eventDateTime ASC";
+    } else if($sort == "eventDateTimeHigh") {
+        $sql = "SELECT * FROM events ORDER BY eventDateTime DESC";
+    } else {
+        $sql = "SELECT * FROM events ORDER BY eventName ASC";
+    }
+    
+} else if($filter != "") {
+    $genre = "'".$filter."'";
+    $sql = "SELECT * FROM movies WHERE movieGenre=$genre";
+} else {
+    $sql = "SELECT * FROM movies";
+}
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
@@ -255,47 +397,6 @@ require('includes/config.php');
     <div class="col-sm-4">
           <div class="card" style="width: 350px">
             <img
-              class="card-img-top poster"
-              src="./uploads/<?php echo $row['eventPoster']; ?>"
-              alt="Card image"
-              style="width: 100%"
-            />
-            <div class="card-body">
-              <h4 class="card-title"><?php echo $row['eventName']; ?></h4>
-              <p class="card-text"><?php echo $row['eventVenue']; ?> | <span class="price"> ₹<?php echo $row['eventPrice']; ?></span></p>
-              <a href="#" class="btn btn-red">Book Now</a>
-              <a href="#" class="btn btn-blue">Know More</a>
-
-            </div>
-          </div>
-    </div>
- 
-<?php }
-} else {
-  echo "<tr><td colspan='7' style='text-align: center;'>No Movies Have Been Added Yet</td></tr>";
-}
-  ?>
-      </div>
-    </div>
-    
-    <!-- Movie Cards -->
-
-    <div class="container text-center event-container">
-      <h3 id="movies" class="event-category">Movies</h3>
-      <br />
-      <div class="row">
-      <?php
-
-$sql = "SELECT * FROM movies";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-  while($row = $result->fetch_assoc()) {
-
-    ?>
-    <div class="col-sm-4">
-          <div class="card" style="width: 350px">
-          <img
               class="card-img-top poster"
               src="./uploads/<?php echo $row['moviePoster']; ?>"
               alt="Card image"
@@ -315,10 +416,10 @@ if ($result->num_rows > 0) {
 } else {
   echo "<tr><td colspan='7' style='text-align: center;'>No Movies Have Been Added Yet</td></tr>";
 }
-  ?>  
-                
+  ?>
       </div>
     </div>
+   
     <br />
 
     <!-- Footer -->
